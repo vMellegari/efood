bbfddf04257444ea41fd6efca34827faa0ebd4cd
Vinicius <vini.a.mellegari@gmail.com>
2025-02-24 12:59:01 -0300

    atualização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,29 @@
-import { Branding, HeaderStyle } from './styles'
 import logo from '../../assets/images/logo.svg'
+import { HeaderStyle } from './styles'
+import { Branding, LinkRestaurantes, TextCart } from './styles'
+import { open } from '../../store/reducers/cart'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
-const Header = () => (
-  <HeaderStyle>
-    <div className="container">
-      <Branding href="/">
-        <img src={logo} alt="Logo" />
-      </Branding>
-    </div>
-  </HeaderStyle>
-)
+const Header = () => {
+  const dispatch = useDispatch()
+  const { pedido } = useSelector((state: RootReducer) => state.cart)
+  const openCart = () => {
+    dispatch(open())
+  }
+  return (
+    <HeaderStyle>
+      <div className="container">
+        <LinkRestaurantes href="/">Restaurantes</LinkRestaurantes>
+        <Branding href="/">
+          <img src={logo} alt="Logo do restaurante" />
+        </Branding>
+        <TextCart onClick={openCart}>
+          {pedido.length} produto(s) no carrinho
+        </TextCart>
+      </div>
+    </HeaderStyle>
+  )
+}
 
 export default Header
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 import fundo from '../../assets/images/fundo.svg'
+import { breakpoints, cores } from '../../styles'
 
 export const HeaderStyle = styled.header`
   background-image: url(${fundo});
+
   align-items: center;
   text-align: center;
 
@@ -10,13 +12,48 @@ export const HeaderStyle = styled.header`
     padding: 39px 0;
     max-width: 1024px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
     margin: 0 auto;
+
+    @media (max-width: ${breakpoints.mobile}) {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    @media (max-width: ${breakpoints.desktop}) {
+      padding: 39px 10px;
+    }
   }
 `
-
 export const Branding = styled.a`
   max-width: 126px;
   width: 100%;
   height: 60px;
+  @media (max-width: ${breakpoints.mobile}) {
+    margin: 15px;
+  }
+`
+export const LinkRestaurantes = styled.a`
+  font-size: 18px;
+  font-weight: bold;
+  text-decoration: none;
+  color: ${cores.vermelho};
+  margin-top: 39px;
+  margin-right: 90px;
+  @media (max-width: ${breakpoints.mobile}) {
+    margin-top: 0;
+  }
+`
+
+export const TextCart = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 39px;
+  cursor: pointer;
+
+  @media (max-width: ${breakpoints.mobile}) {
+    margin-top: 0;
+  }
 `
